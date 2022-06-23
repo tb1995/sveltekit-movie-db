813631d0eaf9dbbd023560056c9e9251f9dbb054
iamrabie <kishmalakamran359@gmail.com>
2022-06-23 14:02:24 +0500

    little styling applied

--- src/components/popMovies.svelte
+++ src/components/popMovies.svelte
@@ -1,6 +1,6 @@
 <script>
 
-    import MovieCard from "./movieCard.svelte";
+    import MovieCard from "./movieCards.svelte";
     export /**
 * @type {any}
 */
@@ -25,8 +25,9 @@
     .popularMovies
     {
         display:grid;
-        grid-template-columns: repeat(autoFit, minmax(250px, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
         grid-column-gap:1rem;
         grid-row-gap:2rem;
     }
+
 </style>
