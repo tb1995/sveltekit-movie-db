937027e84ee76ff178fd81d3eb883e713f053a8a
iamrabie <kishmalakamran359@gmail.com>
2022-06-23 17:05:56 +0500

    movie id not fetching

---
+++ src/routes/movies/[id].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+    
+    // @ts-ignore
+    export async function load( {fetch, params} )
+    {
+      console.log(params.id);
+      const res = await fetch('https://api.themoviedb.org/3/movie/${params.id}}?api_key=00f18f62f1339acd38f45ceabf0531b5&language=en-US');
+      const data = await res.json();
+      //console.log(data);
+      if (res.ok)
+      {
+          return {
+              props: { data }
+          };
+      }
+    }
+</script>
