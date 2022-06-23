7fde765dab63f4ae522ca051ad886cf7078a322d
iamrabie <kishmalakamran359@gmail.com>
2022-06-23 13:05:44 +0500

    first comp of this app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,29 @@
 <script context="module">
-      export async function load( {fetch} )
+    // @ts-ignore
+    export async function load( {fetch} )
+    {
+      const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=00f18f62f1339acd38f45ceabf0531b5&language=en-US&page=1');
+      const data = await res.json();
+      //console.log(data);
+      if (res.ok)
       {
-        const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=00f18f62f1339acd38f45ceabf0531b5&language=en-US&page=1');
-        const data = await res.json();
-        console.log(data);
-
-        if (res.ok)
-        {
-            return {
-                props: { popular: data.results }
-            };
-        }
+          return {
+              props: { popular: data.results }
+          };
       }
-
+    }
 </script>
 
 <script>
 
+    import PopularMovies from '../components/popMovies.svelte';
+    export /**
+* @type {any}
+*/
+     let popular;
+    //console.log(popular);
 </script>
+
+<header>
+    <PopularMovies {popular}/>
+</header>
