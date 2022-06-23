2fdcd47be0c441242853f1bfbdac5137e01c757a
iamrabie <kishmalakamran359@gmail.com>
2022-06-23 14:04:44 +0500

    Movie cards in Grid form

---
+++ src/components/movieCards.svelte
@@ -0,0 +1,55 @@
+<script>
+export /**
+* @type {any}
+*/
+     let movies;
+</script>
+
+
+<div class="movieCard">
+    <img src="{ 'https://image.tmdb.org/t/p/w500' + movies.poster_path}" alt="{movies.title}" />
+
+    
+    <div class="description">
+        <h2>{movies.title}</h2>
+        <p>
+            {movies.release_date}
+        </p>
+    </div>
+</div>
+
+
+<style>
+    img
+    {
+        width:100%;
+        height:50vh;
+        object-fit: cover;
+        border-radius: 1rem;
+        margin-bottom: 1rem;
+    }
+
+    h2{
+        font-size: 0.9rem;
+
+    }
+
+    .description
+    {
+        height:7vh;
+    }
+
+    p
+    {
+        font-size:0.9rem;
+    }
+
+    .movieCard
+    {
+      display:flex;
+      flex-direction: column;
+      justify-content: space-around;
+      padding:1rem;
+    }
+
+</style>
