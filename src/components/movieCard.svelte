2f464f4149040a00728aaa2ff7fa99b7313a912c
iamrabie <kishmalakamran359@gmail.com>
2022-06-23 13:24:56 +0500

    updated files

---
+++ src/components/movieCard.svelte
@@ -0,0 +1,18 @@
+<script>
+    export /**
+* @type {any}
+*/
+     let movies;
+</script>
+
+
+<div class="movieCard">
+    <img src="{ 'https://image.tmdb.org/t/p/w500' + movies.poster_path}" alt="{movies.title}" />
+</div>
+
+<div class="description">
+    <h2>{movies.title}</h2>
+    <p>
+        {movies.release_date}
+    </p>
+</div>
