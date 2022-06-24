c0ce1bcc86119dc161c3a993f7a0ed6752d7df38
iamrabie <kishmalakamran359@gmail.com>
2022-06-24 16:38:33 +0500

    layout file

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import '../styles/global.css';
+  import Nav from '../components/nav.svelte';
+</script>
+<svelte:head>
+    <title>
+       The Movie DB
+    </title>
+</svelte:head>
+
+<Nav />
+
+<main>
+    <slot></slot>
+</main>
