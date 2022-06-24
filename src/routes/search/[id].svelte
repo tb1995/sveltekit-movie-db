56e9316064c1a5776761f059d0708d68150a3402
iamrabie <kishmalakamran359@gmail.com>
2022-06-24 16:36:22 +0500

    search folder

---
+++ src/routes/search/[id].svelte
@@ -0,0 +1,27 @@
+<script context="module">
+import { dataset_dev } from "svelte/internal";
+
+
+    
+    // @ts-ignore
+    export async function load( {fetch, params} )
+    {
+      
+      //console.log(params);
+      const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=00f18f62f1339acd38f45ceabf0531b5&language=en-US&query=${params.id}&page=1&include_adult=false`);
+      
+      const data= await res.json();
+      //console.log(data);
+      if (res.ok)
+      {
+          return {
+             props :{ searchedMovie: data.result}
+          };
+      }
+    }
+</script>
+
+<script>
+    export let searchedMovie;
+    console.log(searchedMovie);
+</script>
